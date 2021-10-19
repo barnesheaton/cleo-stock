caa6beaa983d66bffe8f2f3056a66ca67f7b8a43
barnesheaton <barnesheaton@gmail.com>
2021-10-19 11:29:06 -0600

    remove app import

--- app/tasks.py
+++ app/tasks.py
@@ -4,19 +4,7 @@ import os
 from rq import get_current_job
 from app.main.database import Database
 from app.main.training import trainModel
-from app import app
-
-def example(seconds):
-    job = get_current_job()
-    print('Starting task')
-    for i in range(seconds):
-        job.meta['progress'] = 100.0 * i / seconds
-        job.save_meta()
-        print(i)
-        time.sleep(1)
-    job.meta['progress'] = 100
-    job.save_meta()
-    print('Task completed')
+# from app import app
 
 def example(seconds):
     job = get_current_job()
@@ -37,6 +25,6 @@ def updateTickerTablesTask(*args, **kwargs):
 def trainModelTask(*args, **kwargs):
     model = trainModel()
     filename = 'test_file_one'
-    outfile = open(os.path.join(app.config['MODELS_DR'], filename), 'wb')
-    pickle.dump(model, outfile)
-    outfile.close()
+    # outfile = open(os.path.join(app.config['MODELS_DR'], filename), 'wb')
+    # pickle.dump(model, outfile)
+    # outfile.close()
