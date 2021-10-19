fd0d807a98b9bf916d4059b90193ca356935dcac
barnesheaton <barnesheaton@gmail.com>
2021-10-19 11:36:52 -0600

    comment out trainmodel task stuff

--- app/tasks.py
+++ app/tasks.py
@@ -1,9 +1,7 @@
 import time
-import pickle
-import os
 from rq import get_current_job
 from app.main.database import Database
-from app.main.training import trainModel
+# from app.main.training import trainModel
 # from app import app
 
 def example(seconds):
@@ -22,9 +20,9 @@ def updateTickerTablesTask(*args, **kwargs):
     database = Database()
     database.updateTickerTables(*args, **kwargs)
 
-def trainModelTask(*args, **kwargs):
-    model = trainModel()
-    filename = 'test_file_one'
-    # outfile = open(os.path.join(app.config['MODELS_DR'], filename), 'wb')
-    # pickle.dump(model, outfile)
-    # outfile.close()
+# def trainModelTask(*args, **kwargs):
+#     model = trainModel()
+#     filename = 'test_file_one'
+#     # outfile = open(os.path.join(app.config['MODELS_DR'], filename), 'wb')
+#     # pickle.dump(model, outfile)
+#     # outfile.close()
