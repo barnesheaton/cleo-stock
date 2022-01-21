0c9e0d4603fe9d72f9be739ba1fd4e5ff391a352
barnesheaton <barnesheaton@gmail.com>
2022-01-21 16:44:00 -0700

    start optimizations

--- app/main/training.py
+++ app/main/training.py
@@ -1,3 +1,4 @@
+from unittest import result
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
@@ -7,6 +8,7 @@ import os
 import math
 import pickle
 import itertools
+import time
 
 from app import app, db
 
@@ -42,7 +44,7 @@ def simulate(
     simulation = Simulation(model_id=model_id, date=datetime.today(), start_date=start_date, end_date=end_date, starting_capital=principal, complete=False)
     db.session.add(simulation)   
     db.session.commit()
-    current_date = start_date
+    current_day = start_date
 
     possible_outcomes = getPossibleOutcomes()
     stockModel = StockModel.query.get(model_id)
@@ -50,7 +52,7 @@ def simulate(
 
     databaseTickers = Database().getTickerTablesList()
     modelTickers = stockModel.tickers
-    simulationTickers = utils.xor(databaseTickers, modelTickers)
+    simulationTickers = ['aapl'] or utils.xor(databaseTickers, modelTickers)
 
     # -------- Main Simulation Loop --------
     utils.printLine("Accuracy Verification")
@@ -66,18 +68,18 @@ def simulate(
     positions = {}
     max_buys_per_day = 5
     prospects_on_day = []
-    while current_date <= end_date:
-        utils.printLine(f"Day ({current_date})")
-        nextDay = current_date + timedelta(days=1)
+    while current_day <= end_date:
+        utils.printLine(f"Day ({current_day})")
+        next_day = current_day + timedelta(days=1)
 
         # --- Buying Stage ---
         for ticker in prospects_on_day:
-            ticker_data_on_date = Database().getTickerDataToDate(ticker, current_date, 1)
+            ticker_data_on_date = Database().getTickerDataToDate(ticker, current_day, 1)
             open_price = ticker_data_on_date['open'][0]
             # Can buy at least one share of this prospect
             volume = math.floor((capital / len(prospects_on_day)) / open_price)
             if ticker not in positions and volume > 0:
-                capital += makeTransaction(positions, ticker, open_price, volume, current_date, capital, selling=False)
+                capital += makeTransaction(positions, ticker, open_price, volume, current_day, capital, selling=False)
 
         # --- Selling Stage ---
         portfolio_worth = 0
@@ -85,7 +87,7 @@ def simulate(
             if positions[ticker]['sold']:
                 continue
 
-            ticker_data_on_date = Database().getTickerDataToDate(ticker, current_date, 1)
+            ticker_data_on_date = Database().getTickerDataToDate(ticker, current_day, 1)
             high_price = ticker_data_on_date['high'][0]
             low_price = ticker_data_on_date['low'][0]
             close_price = ticker_data_on_date['close'][0]
@@ -95,48 +97,28 @@ def simulate(
             # TODO make configurable
             stop_loss = 0.95 * price
             target_price = 1.25 * price
-            is_last_day = current_date == end_date
+            is_last_day = current_day == end_date
 
-            if low_price <= stop_loss and date != current_date:
-                capital += makeTransaction(positions, ticker, stop_loss, shares, current_date, capital)
+            if low_price <= stop_loss and date != current_day:
+                capital += makeTransaction(positions, ticker, stop_loss, shares, current_day, capital)
             elif high_price >= target_price:
-                capital += makeTransaction(positions, ticker, target_price, shares, current_date, capital)
+                capital += makeTransaction(positions, ticker, target_price, shares, current_day, capital)
             elif close_price >= target_price or is_last_day:
-                capital += makeTransaction(positions, ticker, close_price, shares, current_date, capital)
+                capital += makeTransaction(positions, ticker, close_price, shares, current_day, capital)
             else:
                 portfolio_worth += close_price * shares
 
         # --- Prediction Stage ---
-        # :Get new prospects for next day's trading, calculate accuracy values for verifaction of model
         prospects_on_day = []
         for ticker in simulationTickers:
-            print(f"[=== Ticker {ticker} ===]")
-            lookbackDF = Database().getTickerDataToDate(ticker, current_date, lookback_period)
-            verificationDF = Database().getTickerDataAfterDate(ticker, nextDay, prediction_period)
-            print('lookbackDF\n', lookbackDF.tail(5))
-            print('verificationDF\n', verificationDF.head(5))
-            if lookbackDF.shape[0] < 21:
-                print("Not enough data in Lookback DF, skipping")
+            result = getTickerAccuracyAndPredictionMetrics(loadedModel, ticker, current_day, lookback_period, prediction_period, possible_outcomes)
+            if not result:
                 continue
-
-            predictionDF = getPredictions(model=loadedModel, dataframe=lookbackDF, possible_outcomes=possible_outcomes, prediction_period=prediction_period)
-            print('predictionDF\n', predictionDF.head(5))
-            maxDiff = getMaxDiffInPrediction(lookbackDF.iloc[-1]['close'], predictionDF, predictionPeriod=prediction_period)
-            print('Max Price Delta :: ', maxDiff)
-            
-            # --- Accuracy Calculations ---
-            predictedOpens = np.array(predictionDF.iloc[-prediction_period:]['open'])
-            verfiedOpens = verificationDF['open'].to_numpy()
-            openPercentages = np.abs((predictedOpens - verfiedOpens) / verfiedOpens)
-            openAccuracy = np.concatenate([openAccuracy, openPercentages], axis=None)
-
-            predictedCloses = np.array(predictionDF.iloc[-prediction_period:]['close'])
-            verfiedCloses = verificationDF['close'].to_numpy()
-            closePercentages = np.abs((predictedCloses - verfiedCloses) / verfiedCloses)
-            closeAccuracy = np.concatenate([closeAccuracy, closePercentages], axis=None)
-
+           
+            openAccuracy = np.concatenate([openAccuracy, result[0]], axis=None)
+            closeAccuracy = np.concatenate([closeAccuracy, result[1]], axis=None)
             # Ticker gained 10% at some point in prediction period. TODO -> make configurable
-            if maxDiff >= 0.1:
+            if result[2] >= 0.1:
                 prospects_on_day.append(ticker)
 
         if (len(prospects_on_day) > max_buys_per_day):
@@ -147,7 +129,7 @@ def simulate(
         print("Portfolio", portfolio_worth)
         print("Net Worth", capital + portfolio_worth)
         print("Positions", list(positions.keys()))
-        current_date = nextDay
+        current_day = next_day
 
     closeAccuracy = np.mean(closeAccuracy)
     openAccuracy = np.mean(openAccuracy)
@@ -157,7 +139,6 @@ def simulate(
     simulation.close_accuracy = closeAccuracy
     simulation.open_accuracy = openAccuracy
     simulation.total_accuracy = totalAccuracy
-
     db.session.commit()
 
     utils.printLine("Results")
@@ -166,6 +147,30 @@ def simulate(
     print('Total Accuracy :: ', totalAccuracy)
 
     return
+
+def getTickerAccuracyAndPredictionMetrics(model, ticker, date, lookback_period, prediction_period, possible_outcomes):
+    tic = time.perf_counter()
+    print(f"[=== Ticker ({ticker}) ===]")
+    lookback_dataframe = Database().getTickerDataToDate(ticker, date, lookback_period)
+    verification_dataframe = Database().getTickerDataAfterDate(ticker, date + timedelta(days=1), prediction_period)
+    if lookback_dataframe.shape[0] < 21:
+        print("Not enough data in Lookback Dataframe, skipping")
+        return None
+    predictionDF = getPredictions(model=model, dataframe=lookback_dataframe, possible_outcomes=possible_outcomes, prediction_period=prediction_period)
+    print()
+    maxDiff = getMaxDiffInPrediction(lookback_dataframe.iloc[-1]['close'], predictionDF, predictionPeriod=prediction_period)
+    # --- Accuracy Calculations ---
+    verfied_opens = verification_dataframe['open'].to_numpy()
+    verfied_closes = verification_dataframe['close'].to_numpy()
+    predicted_opens = np.array(predictionDF.iloc[-prediction_period:]['open'])
+    predicted_closes = np.array(predictionDF.iloc[-prediction_period:]['close'])
+    openPercentages = np.abs((predicted_opens - verfied_opens) / verfied_opens)
+    closePercentages = np.abs((predicted_closes - verfied_closes) / verfied_closes)
+
+    toc = time.perf_counter()
+    print("getTickerAccuracyAndPredictionMetrics Performance :: ", toc - tic)
+
+    return openPercentages, closePercentages, maxDiff
 
 # TODO add absolute value to deltas to account for dips as well
 def getMaxDiffInPrediction(price, dataframe, predictionPeriod=14):
@@ -193,15 +198,16 @@ def getTickerOutlook(possible_outcomes, model_id=3, ticker="aapl", prediction_pe
         prediction_period=prediction_period
     )
 
-def getPredictions(model, dataframe, possible_outcomes, prediction_period=10):
-    p_dataframe = dataframe
-    for index in range(0, prediction_period):
+def getPredictionsOptimized(model, dataframe, possible_outcomes, prediction_period=14):
+    results = np.array([])
+    print("START results", results)
+    for _ in itertools.repeat(None, prediction_period):
         data = getFeatures(p_dataframe)
-
-        high_price = p_dataframe.iloc[-1]['high']
-        # low_price = p_dataframe.iloc[-1]['Low']
-        open_price = p_dataframe.iloc[-1]['open']
+        # Reduce to one iloc
         close_price = p_dataframe.iloc[-1]['close']
+        # high_price = p_dataframe.iloc[-1]['high']
+        # low_price = p_dataframe.iloc[-1]['Low']
+        # open_price = p_dataframe.iloc[-1]['open']
 
         # TODO make this configurable in Model Settings, can be a Boolean on or off for Bollinger Bands
         # 20% chance of flipping the sign of the prediciton
@@ -219,14 +225,60 @@ def getPredictions(model, dataframe, possible_outcomes, prediction_period=10):
         # hit_lower_band = close_price <= (bands['lower'][-1] * 1.04) or open_price <= (bands['lower'][-1] * 1.04)
         hit_upper_band = close_price >= (bands['upper'][-1] * 0.96)
         hit_lower_band = close_price <= (bands['lower'][-1] * 1.04)
-
         if hit_upper_band and delta_close >= 0: mutiplier = -0.5
         if hit_lower_band and delta_close <= 0: mutiplier = -0.5
 
         predicted_open = close_price * (1 + (delta_open))
-        # predicted_d_high = predicted_open * (1 + frac_high)
         predicted_close = predicted_open * (1 + (delta_close * mutiplier))
+        # predicted_d_high = predicted_open * (1 + frac_high)
 
+        # Use numpy here and concat array's instead of DF to improve performance?
+        p_dataframe = pd.concat([p_dataframe, pd.DataFrame({
+            'date': ['date'],
+            'open': [predicted_open],
+            'high': [0],
+            'low': [0],
+            'close': [predicted_close],
+            'adj_close': [0],
+            'volume': [0],
+        })])
+
+    return p_dataframe
+
+def getPredictions(model, dataframe, possible_outcomes, prediction_period=10):
+    p_dataframe = dataframe
+    results = np.array([])
+    for index in range(0, prediction_period):
+        data = getFeatures(p_dataframe)
+        close_price = p_dataframe.iloc[-1]['close']
+        # high_price = p_dataframe.iloc[-1]['high']
+        # low_price = p_dataframe.iloc[-1]['Low']
+        # open_price = p_dataframe.iloc[-1]['open']
+
+        # TODO make this configurable in Model Settings, can be a Boolean on or off for Bollinger Bands
+        # 20% chance of flipping the sign of the prediciton
+        # mutiplier = -1 if np.random.pareto(1) > 1.2 else 1
+        mutiplier = 1
+        bands = analysis.getBollingerBandWidths(
+            p_dataframe['close'].to_numpy())
+        delta_open, delta_close, _ = getPredictedFeatures(
+            model,
+            data,
+            possible_outcomes=possible_outcomes
+        )
+
+        # hit_upper_band = close_price >= (bands['upper'][-1] * 0.96) or open_price >= (bands['upper'][-1] * 0.96)
+        # hit_lower_band = close_price <= (bands['lower'][-1] * 1.04) or open_price <= (bands['lower'][-1] * 1.04)
+        hit_upper_band = close_price >= (bands['upper'][-1] * 0.96)
+        hit_lower_band = close_price <= (bands['lower'][-1] * 1.04)
+        if hit_upper_band and delta_close >= 0: mutiplier = -0.5
+        if hit_lower_band and delta_close <= 0: mutiplier = -0.5
+
+        predicted_open = close_price * (1 + (delta_open))
+        predicted_close = predicted_open * (1 + (delta_close * mutiplier))
+        # predicted_d_high = predicted_open * (1 + frac_high)
+
+        # Use numpy here and concat array's instead of DF to improve performance?
         p_dataframe = pd.concat([p_dataframe, pd.DataFrame({
             'date': ['date'],
             'open': [predicted_open],
@@ -242,7 +294,7 @@ def getPredictions(model, dataframe, possible_outcomes, prediction_period=10):
 def getPredictedFeatures(model, data, possible_outcomes):
     outcome_score = []
     isProduction = app.config['FLASK_ENV'] == 'production'
-    for possible_outcome in tqdm(possible_outcomes, disable=False):
+    for possible_outcome in tqdm(possible_outcomes, disable=isProduction):
         total_data = np.row_stack((data, possible_outcome))
         outcome_score.append(model.score(total_data))
 
